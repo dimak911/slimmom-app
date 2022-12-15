d07a3b5585e53f9de4768fee5d81a0194e459532
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-15 14:49:11 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import { App } from './App';
+import { theme } from 'theme';
+import { ThemeProvider } from 'styled-components';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
