d0a603044359a703a15074cec5fd72b1e451b826
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-12-17 19:56:23 +0200

    1st

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { GlobalStyle } from 'components/GlobalStyle';
+import { DiaryProductListItem } from './components/DiaryProducListItem/DiaryProducListItem';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
+  const productsList = useSelector(state => state.productList.productsDiary);
+  // console.log(productsList);
   return (
     <>
       <GlobalStyle />
-      <div>
-        <h1>Start here</h1>
-      </div>
+      <ul>
+        {/* {productsList.map(item => console.log(item))} */}
+        {productsList.map(item => (
+          <DiaryProductListItem item={item} key={item.id} />
+        ))}
+      </ul>
     </>
   );
 };
--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import { productListReducer } from '../components/DiaryProducListItem/productListSlice';
 
 export const store = configureStore({
   reducer: {
     // put reducer here
+    productList: productListReducer,
   },
 });
