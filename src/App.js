bad4570618b152e11e55d742cca630b2dc027c83
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-18 13:15:38 +0200

    comment modal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { lazy } from 'react';
 import { RestrictedRoute } from 'components/RestrictedRoute';
 import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from 'components/GlobalStyle';
-import { ModalDailyCalorie } from 'components/ModalDailyCalorie/ModalDailyCalorie';
+// import { ModalDailyCalorie } from 'components/ModalDailyCalorie/ModalDailyCalorie';
 
 const RegistrationPage = lazy(() => import('pages/RegistrationPage'));
 
@@ -10,8 +10,7 @@ export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <ModalDailyCalorie />
-      {/* Modal component */}
+      {/* <ModalDailyCalorie /> */}
       <Routes>
         <Route
           path="/signup"
