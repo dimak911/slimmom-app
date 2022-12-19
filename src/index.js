7b7e8f96476b8e43c1debc5da2ae18214531875b
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-19 12:35:10 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <App />
