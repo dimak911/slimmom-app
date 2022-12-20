401945bd376d071190825fd7046243ba72d51648
Yevhenii Kobets <eu.kobets@gmail.com>
2022-12-20 18:43:41 +0200

    del main page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { DiaryDateCalendar } from 'components/DiaryDateCalendar/DiaryDateCalendar';
 
-import { MainPage } from 'pages/MainPage';
-
 const RegistrationPage = lazy(() => import('pages/RegistrationPage'));
 const LoginPage = lazy(() => import('pages/LoginPage'));
 
@@ -14,8 +12,6 @@ export const App = () => {
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-
         <Route
           path="/signup"
           element={
