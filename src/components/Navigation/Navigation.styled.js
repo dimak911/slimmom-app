6cf6e048193662b655597abd01402b73a8e01d45
Artem Makhov <makhovartem23@gmail.com>
2022-12-20 17:59:40 +0200

    fixed navigation component

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,45 +1,66 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const Section = styled.div`
-  background-color: #264061;
-  width: 100%;
-  height: 100vh;
-  padding-top: 60px;
-@media (min-width: 767px) {
-  padding-top: 100px;
-  }
-`
-
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  margin-left: auto;
-  margin-right: auto;
-  width: 280px;
-  height: 112px;
+  align-items: center;
+  padding: 20px 20px 16px;
+  justify-content: space-between;
+  border-bottom: 2px solid ${p => p.theme.colors.input};
 @media (min-width: 767px) {
-  width: 336px;
+  padding: 20px 0 16px;
   }
-@media (min-width: 1200px) {
-
-  }
+@media (min-width: 1279px){
+  padding: 0;
+  border-bottom: none;
+  justify-content: flex-start;
+}
 `
 export const Link = styled(NavLink)`
   text-decoration: none;
-  text-align: center;
   color: ${P => P.theme.colors.secondary};
   font-weight: ${p => p.theme.fontWeights.bold};
-  font-size: ${p => p.theme.fontSizes.s};
+  font-size: ${p => p.theme.fontSizes.xs};
   letter-spacing: 0.04em;
-&:not(:last-child){
-  margin-bottom:28px
-  }
 &:hover{
-  color: ${p => p.theme.colors.white};
+  color: ${p => p.theme.colors.primary};
   }
-@media (min-width: 767px) {
-  font-size: ${p => p.theme.fontSizes.m};
+@media (min-width: 1279px){
+  margin-top:131px;
+  position: relative;
+&:not(:last-child){
+  margin-right:18px;
+  margin-left: 20px;
+  }
+&:not(:last-child)::before{
+  content: '';
+  display: inline-block;
+  width: 2px;
+  height: 32px;
+  background: ${p => p.theme.colors.input};
+  position: absolute;
+  vertical-align: middle;
+  left: -20px;
+  top: 50%;                        
+  transform: translate(0, -50%)    
+  }
   }
 `
 
+export const Logo = styled.div`
+  width: 46px;
+  height: 44px;
+  background-color: tomato;
+  margin-right: 23px;
+@media (min-width: 767px) {
+  width: 162px;
+  height: 44px;
+  margin-right: 320px;
+  }
+@media (min-width: 1279px) {
+  width: 167px;
+  height: 66px;
+  margin-top: 80px;
+  margin-right: 20px;
+  }
+`
