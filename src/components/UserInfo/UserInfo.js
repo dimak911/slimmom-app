022d5c7f391dad716ac6803b976302ac073c87ef
veta41 <74262484+veta41@users.noreply.github.com>
2022-12-22 06:17:09 +0200

    fix UserInfo

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,13 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logOut } from 'redux/auth/operations';
-import { selectUserName, selectIsLoggedIn } from 'redux/auth/selectors';
+import { selectUserName } from 'redux/auth/selectors';
 
 import { useNavigate } from 'react-router-dom';
 import { UserInfoContainer, UserName, Button } from './UserInfo.styled';
 
 export const UserInfo = () => {
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   const userName = useSelector(selectUserName);
   const navigate = useNavigate();
 
@@ -16,13 +16,11 @@ export const UserInfo = () => {
     navigate('/');
   };
   return (
-    isLoggedIn && (
-      <UserInfoContainer>
-        <UserName>{userName}</UserName>
-        <Button type="button" onClick={isLogout}>
-          Exit
-        </Button>
-      </UserInfoContainer>
-    )
+    <UserInfoContainer>
+      <UserName>{userName}</UserName>
+      <Button type="button" onClick={isLogout}>
+        Exit
+      </Button>
+    </UserInfoContainer>
   );
 };
