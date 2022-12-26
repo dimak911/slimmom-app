261f6096878b85476bdfa96acc83b3af0067267f
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-12-26 09:21:52 +0200

    fix

--- src/redux/calculate/operations.js
+++ src/redux/calculate/operations.js
@@ -1,18 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://slim-mom-od0o.onrender.com/api';
-axios.defaults.baseURL = 'http://localhost:3001/api/';
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-
-  unset() {
-    axios.defaults.headers.common.Authorization = ``;
-  },
-};
-
 export const calculation = createAsyncThunk(
   'calculate/calories',
   async (credentials, { rejectWithValue }) => {
@@ -27,11 +15,8 @@ export const calculation = createAsyncThunk(
 
 export const refreshCalories = createAsyncThunk(
   'calculate/refreshCalories',
-  async (_, { getState, rejectWithValue }) => {
-    const state = getState();
-
+  async (_, { rejectWithValue }) => {
     try {
-      token.set(state.auth.token);
       const { data } = await axios.get('/calculate/calories');
       return data;
     } catch (error) {
