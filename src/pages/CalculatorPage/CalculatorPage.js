de88ac8da8abd772cf69af76d4fff59e178db49e
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-12-26 14:01:17 +0200

    add dispatch(refreshCalories) in app

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,17 +1,9 @@
-import React, { useEffect } from 'react';
-import { refreshCalories } from 'redux/calculate/operations';
+import React from 'react';
 import { DailyCaloriesForm } from 'components/DailyCaloriesForm';
 import { SideBar } from 'components/SideBar/SideBar';
 import { WrapForm, WrapSideBar, WrapPage } from './CalculatorPage.styled';
-import { useDispatch } from 'react-redux';
 
 const CalculatorPage = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(refreshCalories());
-  }, [dispatch]);
-
   return (
     <WrapPage>
       <WrapForm>
