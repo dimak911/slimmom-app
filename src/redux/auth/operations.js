11e17ea29b706fe21f476c44dddeb2f6950d704a
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-12-27 20:20:36 +0200

    fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
-axios.defaults.baseURL = 'http://localhost:3001/api/';
-//
 axios.defaults.withCredentials = true;
 
 const token = {
--- src/redux/products/operations.js
+++ src/redux/products/operations.js
@@ -52,14 +52,9 @@ export const removeDiaryListItem = createAsyncThunk(
 
 axios.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
   },
   async function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-
     const {
       response: { status },
     } = error;
